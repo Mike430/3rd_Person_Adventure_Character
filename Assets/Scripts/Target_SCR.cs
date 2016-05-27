c085120627e2dafa60af9c9555ed4c4843dd2511
Mike430 <ymike2045@gmail.com>
2016-05-27 16:50:56 +0100

    Targeting system in place. Move the cross hair over the flying box and see it change colour. The second it's not in the cross hair anymore, it is forgotten by the targeting script.

---
+++ Assets/Scripts/Target_SCR.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target_SCR : MonoBehaviour {
+
+    [SerializeField]
+    private Material _untargeted;
+    [SerializeField]
+    private Material _targeted;
+
+    private bool _isTargeted;
+    private Renderer _rend;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _rend = GetComponent<Renderer>();
+        _rend.enabled = true;
+
+        _isTargeted = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (_isTargeted)
+            _rend.sharedMaterial = _targeted;
+        else
+            _rend.sharedMaterial = _untargeted;
+
+    }
+
+    public void Targeted(bool isTargeted)
+    {
+        _isTargeted = isTargeted;
+    }
+}
---
+++ Assets/Scripts/Targeting_System_SCR.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Targeting_System_SCR : MonoBehaviour {
+
+    private bool _targetFound = false;
+    private GameObject _currentTarget = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit hitInfo;
+        Ray crossHair = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(crossHair, out hitInfo, 50.0f))
+        {
+            if (hitInfo.collider.gameObject.tag == "Target")
+            {
+                _targetFound = true;
+                _currentTarget = hitInfo.collider.gameObject;
+                _currentTarget.GetComponent<Target_SCR>().Targeted(true);
+            }
+            else
+            {
+                TargetNotFound();
+            }
+        }
+        else
+            TargetNotFound();
+	}
+
+    private void TargetNotFound()
+    {
+        if (_currentTarget != null)
+            _currentTarget.GetComponent<Target_SCR>().Targeted(false);
+
+        _targetFound = false;
+        _currentTarget = null;
+    }
+
+    public bool FoundTarget()
+    {
+        return _targetFound;
+    }
+
+    public GameObject GetTarget()
+    {
+        if (_currentTarget != null)
+            return _currentTarget;
+        else
+            return null;
+    }
+}
