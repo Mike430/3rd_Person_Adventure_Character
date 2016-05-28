614c7ec02583501be87f5314274bff140846012a
Mike430 <ymike2045@gmail.com>
2016-05-28 13:37:16 +0100

    Rope drawing. I am now ready to begin programming the player's rope swing.

---
+++ Assets/Scripts/Line_SCR.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Line_SCR : MonoBehaviour {
+
+    public LineRenderer _lnRenderer;
+    public Transform _posA;
+    public Transform _posB;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _lnRenderer.SetPosition(0, _posA.position);
+        _lnRenderer.SetPosition(1, _posB.position);
+    }
+}
